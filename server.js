e40f7011bf26552a4945305dfac1ca6fb01bc1cc
Yoam Farges <yoam.farges@gmail.com>
2021-02-18 22:00:25 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+/*
+Required to run on Jelastic.
+https://strapi.io/documentation/3.0.0-beta.x/getting-started/deployment.html#application-configuration
+*/
+const strapi = require('strapi');
+strapi(/* {...} */).start();
+
